00e4ee7bf9512c0c5ee21bf04cc63c3d0283c170
Sumanth Thikka <sumanth@Sumanths-MacBook-Pro.local>
2016-02-25 03:53:35 -0600

    Code challenge for touchofmodern

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,51 @@
-
+require "vertex"
 class Cuboid
+
+	attr_accessor :origin, :length, :width, :height
+	alias :x :length
+	alias :y :width
+	alias :z :height
   
   #BEGIN public methods that should be your starting point
 
+  # Assuming there's no other data, changing just the origin of the cuboid
   def move_to!(x, y, z)
+  	origin.x = x
+  	origin.y = y
+  	origin.z = z
   end
   
-  def vertices
+  def vertices   
+  	[
+  		Vertex.new(origin.x, origin.y, origin.z),
+			Vertex.new((origin.x + length), origin.y, origin.z),
+			Vertex.new((origin.x + length), (origin.y + width), origin.z),
+			Vertex.new((origin.x + length), (origin.y + width), (origin.z + height)),
+			Vertex.new(origin.x, (origin.y + width), origin.z),
+			Vertex.new(origin.x, (origin.y + width), (origin.z + height)),
+			Vertex.new(origin.x, origin.y, (origin.z + height)),
+			Vertex.new((origin.x + length), origin.y, (origin.z + height))
+		]
   end
   
   #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+  def intersects?(other_cuboid)
+  	(other_cuboid.origin.x < (origin.x + length)) && ((other_cuboid.origin.x + other_cuboid.length) > origin.x) &&
+		(other_cuboid.origin.y < (origin.y + width)) && ((other_cuboid.origin.y + other_cuboid.width) > origin.y) &&
+		(other_cuboid.origin.z < (origin.z + height)) && ((other_cuboid.origin.z + other_cuboid.height) > origin.z)
   end
 
+  # Rotates the cuboid along specified axis in specified direction
+  # Direction can be 'clockwise': away from (0,0,0) and 'anti_clockwise': towards (0,0,0)
+  # Assumption, no other cuboids in clockwise direction
+  def rotate!(axis, direction)
+  	return false unless ['clock', 'anti_clock'].include?(direction.to_s) and ['x', 'y', 'z'].include?(axis.to_s.downcase)
+  	direction_indicator = direction == 'clock' ? 1 : -1
+  	changed_coordinate = origin.send(axis) + (direction_indicator * send(axis))
+  	return false if changed_coordinate < 0
+  	origin.send("#{axis}=", changed_coordinate)
+  	return true
+  end
   #END public methods that should be your starting point  
 end
+
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,4 +1,5 @@
 require 'cuboid'
+require 'vertex'
 
 #This test is incomplete and, in fact, won't even run without errors.  
 #  Do whatever you need to do to make it work and please add your own test cases for as many
@@ -7,11 +8,194 @@ describe Cuboid do
  
   describe "move_to" do
     it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+    	cuboid = Cuboid.new()
+		  cuboid.origin = Vertex.new(0,0,0)
+			cuboid.length = 5
+			cuboid.width = 3
+			cuboid.height = 4
+			cuboid.move_to!(1,2,3)
+      expect(cuboid.origin.x).to eq(1)
+      expect(cuboid.origin.y).to eq(2)
+      expect(cuboid.origin.z).to eq(3)
     end
-  end    
+  end  
+
+  describe "vertices" do
+  	before :each do
+		  @cuboid = Cuboid.new()
+		  @cuboid.origin = Vertex.new(0,0,0)
+			@cuboid.length = 5
+			@cuboid.width = 3
+			@cuboid.height = 4
+		end
+  	it "returns an array" do
+			expect(@cuboid.vertices).to be_a Array
+  	end
+
+  	it "returns an array of 8 elements" do
+  		expect(@cuboid.vertices.length).to eq(8)
+  	end
+  	
+  	it "returns an array of Vertex objects" do
+  		expect(@cuboid.vertices.first).to be_an_instance_of Vertex
+  	end
+  end
+
+  describe "rotate!" do
+  	before :each do
+		  @cuboid = Cuboid.new()
+		  @cuboid.origin = Vertex.new(0,0,0)
+			@cuboid.length = 5
+			@cuboid.width = 3
+			@cuboid.height = 4
+		end
+  	it "rotates clockwise" do
+			expect(@cuboid.rotate!('x', 'clock')).to be true
+  	end
+
+  	it "rotates clockwise" do
+			expect(@cuboid.rotate!('y', 'clock')).to be true
+  	end
+
+  	it "rotates clockwise" do
+			expect(@cuboid.rotate!('z', 'clock')).to be true
+  	end
+
+  	it " can't rotate anti-clockwise" do
+  		expect(@cuboid.rotate!('x', 'anti_clock')).to be false
+  	end
+  	
+  	describe "can't rotate with invalid inputs" do 
+  		before :each do
+			  @cuboid = Cuboid.new()
+			  @cuboid.origin = Vertex.new(0,0,0)
+				@cuboid.length = 5
+				@cuboid.width = 3
+				@cuboid.height = 4
+			end
+
+  		it " can't rotate with invalid axis" do
+				expect(@cuboid.rotate!('xsa', 'anti_clock')).to be false
+			end
+
+			it " can't rotate with invalid direction" do
+				expect(@cuboid.rotate!('x', 'santi_clocks')).to be false
+			end
+  	end
+  	
+  end
   
   describe "intersects?" do
+  	
+  	describe "if origin of the second cuboid is inside first" do
+  		before :each do
+			  @cuboid_1 = Cuboid.new()
+			  @cuboid_1.origin = Vertex.new(0,0,0)
+				@cuboid_1.length = 5
+				@cuboid_1.width = 3
+				@cuboid_1.height = 4
+			end
+  		it "it returns true when second is smaller than first" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(1,1,1)
+				cuboid_2.length = 1
+				cuboid_2.width = 1
+				cuboid_2.height = 1
+				expect(@cuboid_1.intersects?(cuboid_2)).to be true
+  		end
+
+  		it "it returns true when second is equal or bigger than first" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(1,1,1)
+				cuboid_2.length = 5
+				cuboid_2.width = 4
+				cuboid_2.height = 4
+				expect(@cuboid_1.intersects?(cuboid_2)).to be true
+  		end
+  		it "it returns false when second origin is outside first and not intersecting" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(6,6,6)
+				cuboid_2.length = 2
+				cuboid_2.width = 2
+				cuboid_2.height = 2
+				expect(@cuboid_1.intersects?(cuboid_2)).to be false
+  		end
+  	end
+
+  	describe "if origin of the first is farther than the second" do
+  		before :each do
+			  @cuboid_1 = Cuboid.new()
+			  @cuboid_1.origin = Vertex.new(5,5,5)
+				@cuboid_1.length = 5
+				@cuboid_1.width = 3
+				@cuboid_1.height = 4
+			end
+  		it "it returns true when second origin is outside first" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(4,2,3)
+				cuboid_2.length = 4
+				cuboid_2.width = 4
+				cuboid_2.height = 5
+				expect(@cuboid_1.intersects?(cuboid_2)).to be true
+  		end
+
+  		it "it returns true when second origin is outside first" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(5,6,6)
+				cuboid_2.length = 5
+				cuboid_2.width = 4
+				cuboid_2.height = 4
+				expect(@cuboid_1.intersects?(cuboid_2)).to be true
+  		end
+
+  		it "it returns false when second origin is outside first and not intersecting" do
+  			cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(0,0,0)
+				cuboid_2.length = 2
+				cuboid_2.width = 2
+				cuboid_2.height = 2
+				expect(@cuboid_1.intersects?(cuboid_2)).to be false
+  		end
+
+  	end
+
+  	describe "when two cuboids are adjacent" do
+  		before :each do
+			  @cuboid_1 = Cuboid.new()
+			  @cuboid_1.origin = Vertex.new(0,0,0)
+				@cuboid_1.length = 5
+				@cuboid_1.width = 5
+				@cuboid_1.height = 4
+			end
+			it "returns false when vertical surfaces are next to each other" do
+				cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(5,0,0)
+				cuboid_2.length = 2
+				cuboid_2.width = 2
+				cuboid_2.height = 2
+				expect(@cuboid_1.intersects?(cuboid_2)).to be false
+			end
+
+			it "returns false when horizantal surfaces are next to each other" do
+				cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(0,5,0)
+				cuboid_2.length = 2
+				cuboid_2.width = 2
+				cuboid_2.height = 2
+				expect(@cuboid_1.intersects?(cuboid_2)).to be false
+			end
+
+			it "returns false when cuboids are aliged along depth" do
+				cuboid_2 = Cuboid.new()
+  			cuboid_2.origin = Vertex.new(0,0,4)
+				cuboid_2.length = 2
+				cuboid_2.width = 2
+				cuboid_2.height = 2
+				expect(@cuboid_1.intersects?(cuboid_2)).to be false
+			end
+
+		end
+
   end
 
 end
